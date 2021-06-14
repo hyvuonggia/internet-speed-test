62cac9c727827fd20b81c1659f225514801fcd3f
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-06-14 17:43:04 +0200

    Finished

---
+++ main.py
@@ -0,0 +1,26 @@
+import speedtest
+
+def main():
+	test = speedtest.Speedtest()
+
+	print("Loading serverlist...")
+	test.get_servers() # -> get list of servers
+	print("Choosing best server...")
+	best = test.get_best_server() # -> choos best server
+	print(f"Found: {best['host']} located in {best['country']}")
+
+	print("Performing download test...")
+	download_result = test.download()
+	print("Performing upload test...")
+	upload_result = test.upload()
+	ping_result = test.results.ping
+
+	print("================RESULT================")
+	print(f"Download speed: {download_result / 1024 / 1024 * 0.125:.2f} MB/s")
+	print(f"Upload speed: {upload_result / 1024 / 1024 * 0.125:.2f} MB/s")
+	print(f"Ping: {ping_result :.2f} ms")
+	print("======================================")
+	input("ENTER to finish!!!")
+
+if __name__ == '__main__':
+	main()
