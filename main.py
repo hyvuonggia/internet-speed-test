d183af68b9afcd105de30872a190ba0cb31cd460
Vuong Gia Hy <hyvuonggia@gmail.com>
2021-06-15 00:01:15 +0200

    Modified

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ def main():
 	ping_result = test.results.ping
 
 	print("===============RESULT================")
-	print(f"Download speed: {download_result / 1024 / 1024 * 0.125:.2f} MB/s ({download_result / 1024 / 1024}:.2f Mbit/s)")
-	print(f"Upload speed: {upload_result / 1024 / 1024 * 0.125:.2f} MB/s ({upload_result / 1024 / 1024}:.2f Mbit/s)")
+	print(f"Download speed: {download_result / 1024 / 1024 * 0.125:.2f} MB/s ({download_result / 1024 / 1024:.2f} Mbit/s)")
+	print(f"Upload speed: {upload_result / 1024 / 1024 * 0.125:.2f} MB/s ({upload_result / 1024 / 1024:.2f} Mbit/s)")
 	print(f"Ping: {ping_result :.2f} ms")
 	print("======================================")
 	input("ENTER to finish!!!")
